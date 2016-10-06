80fa3b227260fa4ecd4270b4fe49344cc4edb25b
Ed <edvb@yandex.ru>
2016-10-07 01:45:05 +0300

    Create age.py

---
+++ age.py
@@ -0,0 +1,12 @@
+# data['Год Рождения'] is imported as 'object' so we must convert it to float / int
+data['Год Рождения'] = data['Год Рождения'].convert_objects(convert_numeric=True)
+data['Год Рождения']
+
+# data['Отметка времени'] has format like '13.10.2015 17:50:14' so we must extract 'year' 
+
+data['Отметка времени'] = pd.to_datetime(data['Отметка времени'])
+data['year'] = data['Отметка времени'].apply(lambda x: x.year)
+
+# now we get 'age'
+data['age'] = data['year'] - data['Год Рождения']
+data['age']
